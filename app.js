b695bff631a67b25070adbe9833124c24e1e8d3d
Felix <felix.hsieh98@gmail.com>
2023-05-13 17:05:17 +0200

    added buttons when enlarged

--- app.js
+++ app.js
@@ -2,27 +2,57 @@ const mainImg = document.querySelector('.main-img');
 const enlargedImgContainer = document.querySelector('.enlarged-img-container');
 const enlargedImg = document.querySelector('.enlarged-img');
 const thumbImgs = document.querySelectorAll('.thumb');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
-thumbImgs.forEach(img => {
-  img.addEventListener('click', () => {
-    // Set active class to clicked thumbnail
-    thumbImgs.forEach(thumbImg => thumbImg.classList.remove('active'));
-    img.classList.add('active');
+let activeIndex = 0;
 
-    // Update main image and enlarged image
-    mainImg.src = img.src;
-    mainImg.alt = img.alt;
-    enlargedImg.src = img.src;
-    enlargedImg.alt = img.alt;
-  });
+function updateImage(index) {
+    // Update the index
+    activeIndex = index;
+
+    // Get the new image
+    const newImg = thumbImgs[index];
+
+    // Update the main and enlarged images
+    mainImg.src = newImg.src;
+    mainImg.alt = newImg.alt;
+    enlargedImg.src = newImg.src;
+    enlargedImg.alt = newImg.alt;
+
+    // Update the active thumbnail
+    thumbImgs.forEach((thumbImg, thumbIndex) => {
+        thumbImg.classList.remove('active');
+        if (thumbIndex === index) {
+            thumbImg.classList.add('active');
+        }
+    });
+}
+
+thumbImgs.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        updateImage(index);
+    });
+});
+
+prevButton.addEventListener('click', () => {
+  const newIndex = activeIndex === 0 ? thumbImgs.length - 1 : activeIndex - 1;
+  updateImage(newIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  const newIndex = activeIndex === thumbImgs.length - 1 ? 0 : activeIndex + 1;
+  updateImage(newIndex);
 });
 
 mainImg.addEventListener('click', () => {
   enlargedImgContainer.style.display = 'block';
 });
 
-enlargedImgContainer.addEventListener('click', () => {
-  enlargedImgContainer.style.display = 'none';
+enlargedImgContainer.addEventListener('click', (event) => {
+  if (event.target === enlargedImgContainer) {
+      enlargedImgContainer.style.display = 'none';
+  }
 });
 
 function openTab(evt, tabName) {
